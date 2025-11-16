f2259591fa7632138a037c7994b955a25ccb133e
Yaelgpt <carlosyaelacuna08@gmail.com>
2025-11-15 18:10:25 -0800

    Delete src directory

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <h1>🌈 Climas</h1>
-      <h2></h2>
-    </header>
-  );
-}
--- src/components/WeatherCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function WeatherCard({ ciudad, temp, clima, icono, color }) {
-  return (
-    <div className="card" style={{ backgroundColor: color }}>
-      <h3>{icono} {ciudad}</h3>
-      <p className="temp">{temp}</p>
-      <p className="clima">{clima}</p>
-    </div>
-  );
-}
